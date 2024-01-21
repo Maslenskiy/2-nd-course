73a0f8cfa91f2c5992aaef162d1fe009f6aa1511
Yuriy Maslenskiy <maslenski2000@mail.ru>
2024-01-21 13:32:16 +0300

    Задание 3

--- hw-2.7.js
+++ hw-2.7.js
@@ -8,18 +8,34 @@
 // Задание 2
 // Создайте функцию, которая в качестве параметров принимает массив строк и строку. Возвращать данная функция должна новый массив, содержащий только те элементы переданного массива, которые начинаются с переданной строки. Регистр символов не должен влиять.
 
-function searchStart(arr, searchString) {
-    // Приведем строку поиска и все элементы массива к нижнему регистру
-    searchString = searchString.toLowerCase();
-    arr = arr.map(item => item.toLowerCase());
+// function searchStart(arr, searchString) {
+//     // Приведем строку поиска и все элементы массива к нижнему регистру
+//     searchString = searchString.toLowerCase();
+//     arr = arr.map(item => item.toLowerCase());
 
-    // Используем метод filter для отбора элементов, начинающихся с переданной строки
-    const result = arr.filter(item => item.startsWith(searchString));
+//     // Используем метод filter для отбора элементов, начинающихся с переданной строки
+//     const result = arr.filter(item => item.startsWith(searchString));
 
-    return result;
-}
+//     return result;
+// }
 
-// Примеры вызова функции
-console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко')); // ['кошка', 'комар']
-console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру')); // ['груша']
-console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино')); // []
+// // Примеры вызова функции
+// console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко')); // ['кошка', 'комар']
+// console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру')); // ['груша']
+// console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино')); // []
+
+
+// Задание 3
+
+// Округлите число 32.58884:
+
+// До меньшего целого
+
+let num = 32.58884;
+console.log(Math.floor(num));
+
+// До большего целого
+console.log(Math.ceil(num));
+
+// До ближайшего целого
+console.log(Math.round(num));
