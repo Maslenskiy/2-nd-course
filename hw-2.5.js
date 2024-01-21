e912bb80a769109e8f246dffc319366b74c417de
Yuriy Maslenskiy <maslenski2000@mail.ru>
2024-01-21 12:48:01 +0300

    Задача 3.1

--- hw-2.5.js
+++ hw-2.5.js
@@ -11,15 +11,26 @@
 
 // Задача 2
 
-function checkEvenOdd(number) {
-  if (number % 2 === 0) {
-      return 'Число четное';
-  } else {
-      return 'Число нечетное';
-  }
-}
+// function checkEvenOdd(number) {
+//   if (number % 2 === 0) {
+//       return 'Число четное';
+//   } else {
+//       return 'Число нечетное';
+//   }
+// }
 
-// Пример использования функции
-let num = prompt('Введите число: ');
-let result = checkEvenOdd(parseInt(num));
-console.log(result);
+// // Пример использования функции
+// let num = prompt('Введите число: ');
+// let result = checkEvenOdd(parseInt(num));
+// console.log(result);
+
+// Задача 3.1
+
+function printSquare(number) {
+    let square = number * number;
+    console.log('Квадрат числа ' + number + ' равен ' + square);
+  }
+  
+  // Пример использования функции
+  let num = prompt('Введите число: ');
+  printSquare(parseInt(num));
