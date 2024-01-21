9535bbce136c49c9e0c5778e41e37c5c9a37f9f2
Yuriy Maslenskiy <maslenski2000@mail.ru>
2024-01-21 13:31:33 +0300

    Задание 2

--- hw-2.7.js
+++ hw-2.7.js
@@ -1,6 +1,25 @@
 // Задание 1
 // Преобразуйте строку js в верхний регистр JS.
 
-let str = 'привет мир';
-let result = str.toUpperCase();
-console.log(result);
+// let str = 'привет мир';
+// let result = str.toUpperCase();
+// console.log(result);
+
+// Задание 2
+// Создайте функцию, которая в качестве параметров принимает массив строк и строку. Возвращать данная функция должна новый массив, содержащий только те элементы переданного массива, которые начинаются с переданной строки. Регистр символов не должен влиять.
+
+function searchStart(arr, searchString) {
+    // Приведем строку поиска и все элементы массива к нижнему регистру
+    searchString = searchString.toLowerCase();
+    arr = arr.map(item => item.toLowerCase());
+
+    // Используем метод filter для отбора элементов, начинающихся с переданной строки
+    const result = arr.filter(item => item.startsWith(searchString));
+
+    return result;
+}
+
+// Примеры вызова функции
+console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко')); // ['кошка', 'комар']
+console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру')); // ['груша']
+console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино')); // []
