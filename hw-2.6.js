b935fbb70ac79d48dda97a436e965a5872ae1cc9
Yuriy Maslenskiy <maslenski2000@mail.ru>
2024-01-21 13:20:34 +0300

    Задание 9

--- hw-2.6.js
+++ hw-2.6.js
@@ -116,10 +116,24 @@
 // Задание 8
 
 // Дана строка: 'abcdef'.
-// Необходимо, чтобы программа вывела в консоль 'fedcba'
+// // Необходимо, чтобы программа вывела в консоль 'fedcba'
 
-let str = 'abcsef';
+// let str = 'abcsef';
 
-let result = str.split('').reverse().join('');
+// let result = str.split('').reverse().join('');
 
-console.log(result);
+// console.log(result);
+
+// Задание 9
+// Дан массив: 
+// [[1, 2, 3,],[4, 5, 6]].
+
+// Выведите в консоль массив вида: 
+// [1, 2, 3, 4, 5, 6].
+const arr = [[1, 2, 3], [4, 5, 6]];
+
+let flattenedArr = arr.reduce(function (accumulator, currentArray) {
+    return accumulator.concat(currentArray);
+}, []);
+
+console.log(flattenedArr);
