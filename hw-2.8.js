23f20e4a49945204dbf0cb10f9fd856683469628
Yuriy Maslenskiy <maslenski2000@mail.ru>
2024-01-25 18:31:05 +0300

    Задание 2

--- hw-2.8.js
+++ hw-2.8.js
@@ -1,15 +1,48 @@
 // Задание 1
 // С помощью метода массива sort отсортируйте массив people по возрастанию возраста и выведите их в консоль.
 
-const people = [
-   { name: 'Глеб', age: 29 },
-   { name: 'Анна', age: 17 },
-   { name: 'Олег', age: 7 },
-   { name: 'Оксана', age: 47 }
-];
-// Колбэк для сортировки
-function sortByAge (a, b){
-  return  a.age - b.age
-}
-// Вывел в консоль
-console.log(people.sort(sortByAge));
+// const people = [
+//    { name: 'Глеб', age: 29 },
+//    { name: 'Анна', age: 17 },
+//    { name: 'Олег', age: 7 },
+//    { name: 'Оксана', age: 47 }
+// ];
+// // Колбэк для сортировки
+// function sortByAge (a, b){
+//   return  a.age - b.age
+// }
+// // Вывел в консоль
+// console.log(people.sort(sortByAge));
+
+// Задание 2
+function isPositive(num) {
+    return num > 0;
+  }
+  
+  function isMale(person) {
+    return person.gender === 'male';
+  }
+  
+  function filter(arr, ruleFunction) {
+    const output = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (ruleFunction(arr[i])) {
+        output.push(arr[i]);
+      }
+    }
+  
+    return output;
+  }
+  
+  console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
+  
+  const people = [
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
+  ];
+  
+  console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'}, {name: 'Олег', gender: 'male'}]
+  
