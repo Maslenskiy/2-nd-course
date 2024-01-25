39d0b2d36252efd141a951f1285ef870cbed0011
Yuriy Maslenskiy <maslenski2000@mail.ru>
2024-01-25 18:35:33 +0300

    Задание 3

--- hw-2.8.js
+++ hw-2.8.js
@@ -15,34 +15,49 @@
 // console.log(people.sort(sortByAge));
 
 // Задание 2
-function isPositive(num) {
-    return num > 0;
+// function isPositive(num) {
+//     return num > 0;
+//   }
+  
+//   function isMale(person) {
+//     return person.gender === 'male';
+//   }
+  
+//   function filter(arr, ruleFunction) {
+//     const output = [];
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       if (ruleFunction(arr[i])) {
+//         output.push(arr[i]);
+//       }
+//     }
+  
+//     return output;
+//   }
+  
+//   console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
+  
+//   const people = [
+//     { name: 'Глеб', gender: 'male' },
+//     { name: 'Анна', gender: 'female' },
+//     { name: 'Олег', gender: 'male' },
+//     { name: 'Оксана', gender: 'female' }
+//   ];
+  
+//   console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'}, {name: 'Олег', gender: 'male'}]
+  
+
+// Задание 3
+
+let secondsPassed = 0;
+
+const intervalId = setInterval(() => {
+  const currentDate = new Date();
+  console.log(currentDate);
+  secondsPassed += 3;
+
+  if (secondsPassed >= 30) {
+    clearInterval(intervalId);
+    console.log('30 секунд прошло');
   }
-  
-  function isMale(person) {
-    return person.gender === 'male';
-  }
-  
-  function filter(arr, ruleFunction) {
-    const output = [];
-  
-    for (let i = 0; i < arr.length; i++) {
-      if (ruleFunction(arr[i])) {
-        output.push(arr[i]);
-      }
-    }
-  
-    return output;
-  }
-  
-  console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
-  
-  const people = [
-    { name: 'Глеб', gender: 'male' },
-    { name: 'Анна', gender: 'female' },
-    { name: 'Олег', gender: 'male' },
-    { name: 'Оксана', gender: 'female' }
-  ];
-  
-  console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'}, {name: 'Олег', gender: 'male'}]
-  
+}, 3000);
